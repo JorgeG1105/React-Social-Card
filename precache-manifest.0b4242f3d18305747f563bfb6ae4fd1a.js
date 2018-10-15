8f5b3b30321107c41e9ce0cb2a9ac8c385896cd9
Jorge Goris <J_goris@live.com>
2018-10-15 20:14:02 +0100

    Updates

---
+++ precache-manifest.0b4242f3d18305747f563bfb6ae4fd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Social-Card/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d624c0a83c749ef7b7bf",
+    "url": "/React-Social-Card/static/js/runtime~main.d624c0a8.js"
+  },
+  {
+    "revision": "871485fb152dcabf490d",
+    "url": "/React-Social-Card/static/js/main.871485fb.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/React-Social-Card/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "871485fb152dcabf490d",
+    "url": "/React-Social-Card/static/css/main.403ba4ef.chunk.css"
+  },
+  {
+    "revision": "66ff0aac0ff849d8f7ec5fb9c10b18cf",
+    "url": "/React-Social-Card/index.html"
+  }
+];
