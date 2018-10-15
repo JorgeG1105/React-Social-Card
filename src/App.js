1f826f2b76ac5ab46b6fe9a691edc5fa661ab97b
JorgeG1105 <J_goris@live.com>
2018-10-15 20:52:35 +0100

    Added components and css files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import CardHeader from './Components/CardHeader'
 
 class App extends Component {
+  state = {
+    cardData: [
+      {id: 1, icon: '', name:'', tag:'', date: '', content: '', image: '', likes:'', comments: ''}
+    ]
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <CardHeader/>
     );
   }
 }
---
+++ src/Components/CardHeader.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const CardHeader = () => {
+	return (
+		<div className ='cardContainer'>
+			<h1>Hello from CardHeader.js!</h1>
+		</div>
+	)
+}
+
+export default CardHeader;
